874dea61f701cad0f29b69f87ce314aa5e9e46e9
shin <shin@shins-MacBook-Air.local>
2024-11-28 00:50:13 +0700

    imma kill myself ++

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # database_project
+
+Run docker compose up to get the database started
+uncomment the first line in init.sql then run it, i cannot find a way to chekc for enum
--- app.py
+++ app.py
@@ -1,21 +1,45 @@
 import flet as ft
 import psycopg2
+from datetime import datetime
 
-conn = psycopg2.connect(database = "postgres",
-                        user = "root",
-                        host= 'localhost',
-                        password = "dbpass",
-                        port = 5432)
-cur = conn.cursor()
-
-from flask import Flask, url_for, render_template, request
+from flask import Flask, url_for, render_template, request, flash, redirect
+from sqlalchemy import text
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
 
 app = Flask(__name__)
+
+conn = psycopg2.connect(database="postgres",
+                                        user="root",
+                                        host='localhost',
+                                        password="dbpass",
+                                        port=5432)
+cur = conn.cursor()
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/shift/', methods=('GET', 'POST'))
+def shift():
+    if request.method == 'POST':
+        id = request.form['id']
+
+        if not id:
+            flash('ID is required!')
+        else:
+            t = datetime.now().hour
+            day = datetime.now().date().isoformat()
+            shift = 2
+            if 8 <= t <=14:
+                shift = 1
+                cur.execute("INSERT INTO shift (date, employee_id, shift_number) values (%s, %s, %s)", (day, id, shift))
+                conn.commit()
+            return redirect(url_for('index'))
+
+    return render_template('shift.html')
+
 
 @app.route('/login')
 def login():
