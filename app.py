2d8d6668495e554370af1c8f0fcc0f5e94312744
shin <shin@shins-MacBook-Air.local>
2024-11-28 13:08:37 +0700

    changed .env to work with render.com

--- app.py
+++ app.py
@@ -5,14 +5,15 @@ from datetime import datetime
 from flask import Flask, url_for, render_template, request, flash, redirect
 from sqlalchemy import text
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, func
+from sqlalchemy.orm import Session
 
 app = Flask(__name__)
 
-conn = psycopg2.connect(database="postgres",
-                                        user="root",
-                                        host='localhost',
-                                        password="dbpass",
+conn = psycopg2.connect(database="pharmacy_db_loob",
+                                        user="pharmacy_db_loob_user",
+                                        host='dpg-ct22pcm8ii6s73fk6lo0-a.oregon-postgres.render.com',
+                                        password="gFTd7aJRHHb8PYJ6FqcqDOomdSwkQbjv",
                                         port=5432)
 cur = conn.cursor()
 
@@ -25,17 +26,21 @@ def index():
 def shift():
     if request.method == 'POST':
         id = request.form['id']
-
+        name = request.form['name']
+        #cur.execute("SELECT * FROM employee WHERE id = %s AND employee_name = %s", (id, name))
+        #exists = cur.fetchone()
         if not id:
             flash('ID is required!')
+        #if exists == None:
+            #flash('Youre not an employee')
         else:
             t = datetime.now().hour
             day = datetime.now().date().isoformat()
             shift = 2
             if 8 <= t <=14:
                 shift = 1
-                cur.execute("INSERT INTO shift (date, employee_id, shift_number) values (%s, %s, %s)", (day, id, shift))
-                conn.commit()
+            cur.execute("INSERT INTO shift (date, employee_id, shift_number) values (%s, %s, %s)", (day, id, shift))
+            conn.commit()
             return redirect(url_for('index'))
 
     return render_template('shift.html')
